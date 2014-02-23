54c7ae1f952f3703f719cbdd0f8cd562af1f29ab
David Vilar <summun@gmail.com>
2014-02-23 12:22:14 +0000

    formatting numerics

--- app/parser.rb
+++ app/parser.rb
@@ -1,45 +1,109 @@
 class Parser
-  def parse json, indent = ''
-    hash = json.is_a?(Hash) ? json : JSON.parse(json)
+  def parse input, indent = ''
+    json = JSON.parse(input)
 
-    hash.inject ['{'] do |memo,(k,v)|
-      memo.push format_field k, v, indent
-      memo
-    end.push("#{indent}}").flatten
+    format(json).flatten
   end
 
   private
 
-  def format_field key, value, indent
-    line = "#{indent}\t#{quote(key)}:"
-
-    if value.is_a? Hash
-      format_hash(line, value, indent)
-    elsif value.is_a? Array
-      format_array(line, value)
+  def format element, indent = ''
+    if element.is_a? Hash
+      format_hash(element, indent)
+    elsif element.is_a? Array
+      format_array(element, indent)
+    elsif element.is_a? Numeric
+      [element.to_s]
     else
-      ["#{line} #{quote(value)}"]
+      [quote(element)]
     end
   end
 
-  def format_hash line, value, indent
-    nested_lines = parse(value, indent + "\t")
-    nested_lines[0] = "#{line} #{nested_lines[0]}"
-    nested_lines
+  def format_hash hash, indent
+    lines = ['{']
+    keys = hash.keys
+    unless keys.empty?
+      last_element_key = keys.pop
+      nested_indent = "#{indent}\t"
+      keys.each { |k| lines.push format_hash_element(k, hash[k], nested_indent) }
+      lines.push format_hash_element(last_element_key, hash[last_element_key], nested_indent, true)
+    end
+    lines.push "#{indent}}"
+    lines
   end
 
-  def format_array line, value
-    avalue = if value.empty?
-      ''
-    else
-      value.drop(1).inject(quote(value[0])) do |memo, elem|
-        "#{memo}, #{quote(elem)}"
-      end
+  def format_hash_element key, value, indent, last = false
+    lines = format(value, indent)
+    lines[0] = "#{format_key(key, indent)}#{lines[0]}"
+    return lines if last
+    lines[-1] = "#{lines[-1]},"
+    lines
+  end
+
+  def format_key key, indent
+    "#{indent}#{quote key}: "
+  end
+
+  def format_array array, indent
+    lines = ['[']
+    unless array.empty?
+      last_value = array.pop
+      nested_indent = "#{indent}\t"
+      array.each { |v| lines.push format_array_element(v, nested_indent) }
+      lines.push format_array_element(last_value, nested_indent, true)
     end
-    ["#{line} [#{avalue}]"]
+    lines.push "#{indent}]"
+    lines
+  end
+
+  def format_array_element value, indent, last = false
+    lines = format(value, indent)
+    lines[0] = "#{indent}#{lines[0]}"
+    return lines if last
+    lines[-1] = "#{lines[-1]},"
+    lines
   end
 
   def quote value
     "\"#{value}\""
   end
+
+
+
+  # end
+
+  # private
+
+  # def format_field key, value, indent
+  #   line = "#{indent}\t#{quote(key)}:"
+
+  #   if value.is_a? Hash
+  #     format_hash(line, value, indent)
+  #   elsif value.is_a? Array
+  #     format_array(line, value)
+  #   else
+  #     ["#{line} #{quote(value)}"]
+  #   end
+  # end
+
+  # def format_hash line, value, indent
+  #   nested_lines = parse(value, indent + "\t")
+  #   nested_lines[0] = "#{line} #{nested_lines[0]}"
+  #   nested_lines
+  # end
+
+  # def format_array line, value
+  #   avalue = if value.empty?
+  #     ''
+  #   else
+  #     value.drop(1).inject(quote(value[0])) do |memo, elem|
+  #       "#{memo}, #{quote(elem)}"
+  #     end
+  #   end
+  #   ["#{line} [#{avalue}]"]
+  # end
+
+  # def quote value
+  #   "\"#{value}\""
+  # end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,25 +1,85 @@
 describe 'Parser' do
-  it 'parses strings' do
-    Parser.new.parse('{"ola":"ke ase"}').should == ['{', "\t\"ola\": \"ke ase\"",'}']
+  context 'hashes' do
+    it 'parses empty hashes' do
+      expected = <<-eos
+{
+}
+    eos
+      expected = expected.split("\n")
+      Parser.new.parse('{}').should == expected
+    end
+
+    it 'parses hashes' do
+      expected = <<-eos
+{
+\t"key1": "value 1",
+\t"key2": "value 2"
+}
+      eos
+      expected = expected.split("\n")
+      Parser.new.parse('{"key1":"value 1","key2":"value 2"}').should == expected
+    end
+
+    it 'parses nested hashes' do
+      expected = <<-eos
+{
+\t"key1": {
+\t\t"key2": "value 2"
+\t}
+}
+      eos
+      expected = expected.split("\n")
+      Parser.new.parse('{"key1":{"key2":"value 2"}}').should == expected
+    end
   end
 
-  it 'parses empty hashes'
+  context 'arrays' do
+    it 'parses empty arrays' do
+      expected = <<-eos
+[
+]
+    eos
+      expected = expected.split("\n")
+      Parser.new.parse('[]').should == expected
+    end
 
-  it 'parses hashes' do
-    Parser.new.parse('{"ola":{"ke":"ase"}}').should == ['{', "\t\"ola\": {","\t\t\"ke\": \"ase\"","\t}",'}']
+    it 'parses arrays' do
+      expected = <<-eos
+[
+\t"value 1",
+\t"value 2"
+]
+      eos
+      expected = expected.split("\n")
+      Parser.new.parse('["value 1","value 2"]').should == expected
+    end
+
+    it 'parses nested arrays' do
+      expected = <<-eos
+[
+\t"value 1",
+\t[
+\t\t"value 2",
+\t\t"value 3"
+\t]
+]
+      eos
+      expected = expected.split("\n")
+      Parser.new.parse('["value 1", ["value 2","value 3"]]').should == expected
+    end
   end
 
-  it 'parses empty arrays' do
-    Parser.new.parse('{"ola":[ ]}').should == ['{', "\t\"ola\": []",'}']
+  it 'parses numbers' do
+    expected = <<-eos
+[
+\t1.234
+]
+      eos
+    expected = expected.split("\n")
+    Parser.new.parse('[1.234]').should == expected
   end
 
-  it 'parses arrays' do
-    Parser.new.parse('{"ola":["ke","ase"]}').should == ['{', "\t\"ola\": [\"ke\", \"ase\"]",'}']
-  end
+  # it 'parses booleans'
 
-  it 'parses numbers'
-
-  it 'parses booleans'
-
-  it 'parses nil'
+  # it 'parses nil'
 end
