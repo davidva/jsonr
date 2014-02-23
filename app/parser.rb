0495a1954ffdb49f8b0b6bb7cd929a5d3bb44304
David Vilar <summun@gmail.com>
2014-02-23 12:22:52 +0000

    removing commented lines

--- app/parser.rb
+++ app/parser.rb
@@ -67,43 +67,4 @@ class Parser
   def quote value
     "\"#{value}\""
   end
-
-
-
-  # end
-
-  # private
-
-  # def format_field key, value, indent
-  #   line = "#{indent}\t#{quote(key)}:"
-
-  #   if value.is_a? Hash
-  #     format_hash(line, value, indent)
-  #   elsif value.is_a? Array
-  #     format_array(line, value)
-  #   else
-  #     ["#{line} #{quote(value)}"]
-  #   end
-  # end
-
-  # def format_hash line, value, indent
-  #   nested_lines = parse(value, indent + "\t")
-  #   nested_lines[0] = "#{line} #{nested_lines[0]}"
-  #   nested_lines
-  # end
-
-  # def format_array line, value
-  #   avalue = if value.empty?
-  #     ''
-  #   else
-  #     value.drop(1).inject(quote(value[0])) do |memo, elem|
-  #       "#{memo}, #{quote(elem)}"
-  #     end
-  #   end
-  #   ["#{line} [#{avalue}]"]
-  # end
-
-  # def quote value
-  #   "\"#{value}\""
-  # end
 end
